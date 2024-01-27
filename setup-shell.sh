c4636736bd5ed3fafe48bdeaa06000a900ad1d32
Le_SaRo <24613248+LeSaRo@users.noreply.github.com>
2024-01-27 21:24:42 +0100

    Isolated powerline segments, Added .env configuration in install script

--- setup-shell.sh
+++ setup-shell.sh
@@ -15,8 +15,21 @@ function ask() {
 # Install zshrc
 if ask "Copy .zshrc"; then
 	ln -s $(realpath "$USHDIR"/shell/zshrc) "$HOME"/.zshrc
-    if ask "Enable colored man pages"; then
-        git clone https://github.com/ael-code/zsh-colored-man-pages.git "$HOME"/.zsh/plugins/zsh-colores-man-pages
+	if ask "Enable colored man pages"; then
+		git clone https://github.com/ael-code/zsh-colored-man-pages.git "$HOME"/.zsh/plugins/zsh-colores-man-pages
+	fi
+
+	if ask "Config shell .env"; then
+		# data drive
+		read -rp "data drive path" RESPONSE
+		echo "USH_DIR_DATA=""$RESPONSE" >>"$USHDIR"/shell/.env-shell
+		# developement
+		read -rp "developement path" RESPONSE
+		echo "USH_DIR_DEV=""$RESPONSE" >>"$USHDIR"/shell/.env-shell
+		# notetaker
+		read -rp "notetaker path" RESPONSE
+		echo "USH_DIR_NOTETAKER=""$RESPONSE" >>"$USHDIR"/shell/.env-shell
+	fi
 fi
 
 # Alacritty config
@@ -35,8 +48,22 @@ if ask "Install tmux config"; then
 	mkdir -p "$CONFDIR"/tmux-powerline/segments
 	ln -s "$(realpath "$USHDIR"/tmux/powerline/config.sh)" "$CONFDIR"/tmux-powerline/config.sh
 	ln -s "$(realpath "$USHDIR"/tmux/powerline/theme.sh)" "$CONFDIR"/tmux-powerline/themes/user.sh
-	ln -s "$(realpath "$USHDIR"/tmux/powerline/text.sh)" "$CONFDIR"/tmux-powerline/segments/text.sh
-	ln -s "$(realpath "$USHDIR"/tmux/powerline/modes.sh)" "$CONFDIR"/tmux-powerline/segments/modes.sh
+	ln -s "$(realpath "$USHDIR"/tmux/powerline/segments/)" "$CONFDIR"/tmux-powerline/
+
+	if ask "Config tmux .env"; then
+		# Alt colors
+		C_BASE="$(tput setaf 21)█$(tput setaf 27)█$(tput setaf 33)█"
+		C_ALT="$(tput setaf 81)██$(tput setaf 218)██"
+		C_WHITE="$(tput setaf 7)"
+		read -rp "Use alt colors (0=${C_BASE} 1={$C_ALT}$(tput setaf 7))" RESPONSE
+		echo "USH_TPL_TH_ALT=""$RESPONSE" >>"$USHDIR""/tmux/.env"
+
+		# Weather cordinates
+		read -rp "Location latitude" RESPONSE
+		echo "USH_TPL_W_LAT=""$RESPONSE" >>"$USHDIR""/tmux/.env"
+		read -rp "Location longitude" RESPONSE
+		echo "USH_TPL_W_LON=""$RESPONSE" >>"$USHDIR""/tmux/.env"
+	fi
 fi
 
 # Neovim (LazyVim) config
