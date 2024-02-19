a49ab69420d704c9983d4abd65639a4b9df7bee1
Le_SaRo <24613248+LeSaRo@users.noreply.github.com>
2024-02-19 23:00:57 +0100

    Made zsh plugins path agnostic, Made vsc_info staged changes green, added PATH

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,40 +1,40 @@
 #!/bin/sh
-# Get env files
-source "$HOME""/ushell/shell/.env-shell"
 
 # Overrides
 alias ls="lsd --color=auto --group-dirs=first"
 alias toilet="toilet -d /usr/share/figlet/fonts"
 
-#Terminal
+# Terminal
 alias p="sudo pacman"
 alias sp="pacman -Ss"
 alias c="clear"
 alias m="man"
 alias mkd="mkdir -v"
 alias hist="history"
+alias zshrl="source $HOME/.zshrc"
+## ls
 alias la="ls -a"
 alias ll="ls -lh"
 alias lla="ll -a"
 alias lt="ls --tree"
 alias lta="lt -a"
+## Directories
 alias cdda="cd ""$USH_DIR_DATA"
 alias cdde="cd ""$USH_DIR_DEV"
 alias cdnt="cd ""$USH_DIR_NOTETAKER"
-alias zshrl="source $HOME/.zshrc"
 alias notetaker="v $USH_DIR_NOTETAKER"
-
-#Config
+## Config
 alias ush="v $HOME/ushell"
 alias ushal="v $HOME/ushell/alacritty/"
 alias ushnv="v $HOME/ushell/nvim/"
 alias ushsh="v $HOME/ushell/shell/"
 alias ushtmux="v $HOME/ushell/tmux/"
 
-#Developement
+# Developement
 alias v="nvim"
 alias sv="sudo nvim"
 alias ged="gedit"
+alias venv="source ""$USH_DIR_VENV""/bin/activate"
 ## Git
 alias g="git"
 alias gs="git status"
@@ -47,13 +47,13 @@ alias gps="git push"
 alias mkds="mkdocs serve"
 alias mkdb="MKD_PDF_EXPORT=1 mkdocs build"
 
-#Internet
+# Internet
 alias yt="yt-dlp -f best -ciw -o \"%(title)s.%(ext)s\" -v" #DL YT vid as MP4
 alias yta="yt-dlp --add-metadata -xic --audio-format mp3"  #DL YT vid audio as MP3
 
 # Games
 alias thprac-16="WINEPREFIX=/media/data/SteamLibrary/steamapps/compatdata/745880/pfx/ $HOME/.local/share/Steam/compatibilitytools.d/GE-Proton8-23/files/bin/wine /media/data/Jeux/Touhou/thprac/thprac.v2.2.1.4.exe"
 
-#Silly
+# Silly
 alias TheSenseOfLife="help 42"
 alias hack="cmatrix"
