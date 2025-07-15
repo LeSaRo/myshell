9e15ad58819ce1f321233773b700eb0255d56994
Le_SaRo <=>
2025-07-15 23:22:21 +0200

    Added kitty config, Updated scripts, Updated setup script

--- setup-shell.sh
+++ setup-shell.sh
@@ -10,8 +10,9 @@ MYSHDIR="$HOME""/myshell"
 CONFDIR="$HOME""/.config"
 
 function ask() {
-	read -rp "$1? (Y/n): " response
-	[ -z "$response" ] || [ "$response" = "y" ]
+	read -n 1 -rp "$1? (Y/n): " response
+    echo
+	[ -z "$response" ] || [ "$response" = "y" ] || [ "$response" = "Y" ]
 }
 
 # Install zshrc
@@ -47,16 +48,39 @@ fi
 
 # Local scripts
 if ask "Install local programs"; then
+    mkdir -pv "$HOME/.local/bin"
 	ln -s $(realpath "$MYSHDIR"/shell/bin/statusbar) "$HOME"/.local/bin/
 	ln -s $(realpath "$MYSHDIR"/shell/bin/dmenu-scripts) "$HOME"/.local/bin/
+	ln -s $(realpath "$MYSHDIR"/shell/bin/rofi-scripts) "$HOME"/.local/bin/
 fi
 
-# Alacritty config
-if ask "Install alcritty config"; then
-	mkdir -p "$CONFDIR""/alacritty/themes"
-	ln -s "$(realpath "$MYSHDIR"/terminal/alacritty.toml)" "$CONFDIR"/alacritty/alacritty.toml
-	wget -O "$CONFDIR"/alacritty/themes/"theme.toml" https://raw.githubusercontent.com/alacritty/alacritty-theme/master/themes/campbell.toml
-fi
+# Terminal config
+while [[ "$ttych" == "" ]]; do
+    read -n 1 -p "Choose terminal (Alacritty/Kitty(D)/None): " ttych
+    echo
+    case $ttych in
+        a|A) 
+            echo "Seting up Alacritty"
+            mkdir -p "$CONFDIR""/alacritty/themes"
+            ln -s "$(realpath "$MYSHDIR"/terminal/alacritty.toml)" "$CONFDIR"/alacritty/alacritty.toml
+            wget -O "$CONFDIR"/alacritty/themes/"theme.toml" https://raw.githubusercontent.com/alacritty/alacritty-theme/master/themes/campbell.toml
+            ttych="a"
+        ;;
+        k|K|"") 
+            echo "Seting up Kitty"
+	        ln -s $(realpath "$MYSHDIR/shell/config/kitty") "$CONFDIR/"
+            ttych="k"
+        ;;
+        n|N) 
+            echo "Ignoring"
+            ttych="n"
+        ;;
+        *) 
+            echo "Invalid"
+            ttych=""
+        ;;
+    esac
+done
 
 # tmux config
 if ask "Install tmux config"; then
